437545bd0817eb6ed396e3d360da4d95232ee633
Masnoon Junaid <masnoon20@gmail.com>
2018-11-13 23:58:37 +0530

    state declaration work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import SearchPage from './SearchPage';
 import MainPage from './MainPage'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    showSearchPage: false
+    books: []
   }
 
-  render() {
+   componentDidMount() {
+     BooksAPI.getAll().then((books) => {
+       this.setState({books:books})
+     })
+   }
+   render() {
     return (
       <div className="app">
-      <MainPage/>
+      <MainPage
+        books= {this.state.books}
+      />
       </div>
     )
   }
